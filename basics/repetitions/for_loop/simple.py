85c13c50fcf5f630d4e1dee1195ec6a5107e2df9
repl.it user <replituser@example.com>
2020-10-18 12:52:57 +0000

    Added code to demonstrate the use of a smiple for loop.

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,17 @@
+
+("How many mountains should I display?")
+mountains = int(input())
+
+print("\nDisplaying...")
+
+for mountain in range(mountains):
+print("""
+           __
+          /  \\_  
+         /^    \\
+        /  ^    \\_
+      _/ ^ ^     ^\\
+     /  ^     ^    \\
+
+
+     """)
