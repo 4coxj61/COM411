5902000ab08c2571b639c14ea22335f8674a2905
repl.it user <replituser@example.com>
2020-11-07 14:08:37 +0000

    Added code to demonstrate a simple dictionary.

--- data/sets/sorted_list.py
+++ data/sets/sorted_list.py
@@ -26,13 +26,11 @@ def run():
   observations = observed()
   remove_observations(observations)
 
-  # populate set
   observations_set = set()
   for observation in observations:
     data = (observation, observations.count(observation))
     observations_set.add(data)
 
-  # display set
   for data in sorted(observations_set):
     print(f"{data[0]} observed {data[1]} times.")
 
